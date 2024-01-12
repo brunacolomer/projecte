940e325e4bae6952b8e0f966bad4f524e8cc33c2
Bruna <bruna.colomer@estudiantat.upc.edu>
2024-01-12 18:14:29 +0100

    12-1 Dialog final

--- dialog.cpp
+++ dialog.cpp
@@ -1,3 +1,32 @@
 #include "dialog.hpp"
-void dialog::dialog(easy_dial& easy, const string& input, 
-              vector<string>& answers, nat& numtelf) throw(){}
+void dialog::dialog(easy_dial& easy, const string& input, vector<string>& answers, nat& numtelf) throw(){
+    string nom;
+    nom = easy.inici();
+    answers.push_back(nom);
+    bool err  = false;
+    nat i = 0;
+    while(i<input.size()){
+        char c = input[i];
+        try{
+            if(c==phone::DELETECHAR) 
+                nom = easy.anterior();
+            else if(c==phone::ENDCHAR)
+                nom = easy.seguent('\0');
+            else
+                nom = easy.seguent(c);
+            answers.push_back(nom);
+        }
+        catch (error e) {
+            answers.push_back(e.mensaje());
+            err = true;
+        }
+        i++;
+    }
+    try{
+        numtelf = easy.num_telf();
+    } catch (error e){
+        numtelf = 0;
+        if (not err) answers.push_back(e.mensaje());
+    }
+    
+}
